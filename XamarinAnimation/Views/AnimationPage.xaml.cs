07e00236f3db048025bd788d2aaaeae6bbcbda7d
Ralph Renzo Pitogo <ralph.pitogo@appstone.tech>
2017-10-16 10:52:23 +0800

    Added composite animation

--- XamarinAnimation/Views/AnimationPage.xaml.cs
+++ XamarinAnimation/Views/AnimationPage.xaml.cs
@@ -25,7 +25,7 @@ namespace XamarinAnimation.Views
             animationTypes.Add(new AnimationTypeModel { Image = "anim_01", Name = "Translation", Index = 6, Details = "This code animates the Image instance by translating it horizontally and vertically over 1 second (1000 milliseconds). The TranslateTo method simultaneously translates the image 100 pixels to the left, and 100 pixels upwards. This is because the first and second arguments are both negative numbers. Providing positive numbers would translate the image to the right, and down." });
             animationTypes.Add(new AnimationTypeModel { Image = "anim_01", Name = "Fading", Index = 7, Details = "This code animates the Image instance by fading it in over 4 seconds (4000 milliseconds). The FadeTo method obtains the current Opacity property value for the start of the animation, and then fades in from that value to its first argument (1)." });
             animationTypes.Add(new AnimationTypeModel { Image = "anim_01", Name = "Compound Animations", Index = 8, Details = "In this example, the Image is translated over 6 seconds (6000 milliseconds). The translation of the Image uses five animations, with the await operator indicating that each animation executes sequentially. Therefore, subsequent animation methods execute after the previous method has completed." });
-
+            animationTypes.Add(new AnimationTypeModel { Image = "anim_01", Name = "Composite Animations", Index = 9, Details = "In this example, the Image is scaled and simultaneously rotated over 4 seconds (4000 milliseconds). The scaling of the Image uses two sequential animations that occur at the same time as the rotation. The RotateTo method executes without an await operator and returns immediately, with the first ScaleTo animation then beginning. The await operator on the first ScaleTo method call delays the second ScaleTo method call until the first ScaleTo method call has completed. At this point the RotateTo animation is half way completed and the Image will be rotated 180 degrees. During the final 2 seconds (2000 milliseconds), the second ScaleTo animation and the RotateTo animation both complete." });
             //animationTypes.Add(new AnimationTypeModel { Image = "anim_01", Name = "", Index = 3, Details = ""});
 
 			labelDetails.Text = "The animation extension methods in the ViewExtensions class are all asynchronous and return a Task<bool> object. The return value is false if the animation completes, and true if the animation is cancelled. Therefore, the animation methods should typically be used with the await operator, which makes it possible to easily determine when an animation has completed. In addition, it then becomes possible to create sequential animations with subsequent animation methods executing after the previous method has completed. For more information, see Compound Animations.\n\nIf there's a requirement to let an animation complete in the background, then the await operator can be omitted. In this scenario, the animation extension methods will quickly return after initiating the animation, with the animation occurring in the background. This operation can be taken advantage of when creating composite animations";
@@ -67,6 +67,11 @@ namespace XamarinAnimation.Views
 					await imageDisplay.TranslateTo(50, 50, 2000);   // Move image diagonally down and right
 					await imageDisplay.TranslateTo(0, 50, 1000);     // Move image left
 					await imageDisplay.TranslateTo(0, 0, 1000);       // Move image up
+					break;
+				case 9:
+					await imageDisplay.RotateTo(360, 4000);
+					await imageDisplay.ScaleTo(1.5, 2000);
+					await imageDisplay.ScaleTo(1, 2000);
 					break;
             }
         }
